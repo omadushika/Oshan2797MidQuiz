8bd2ecf61271dece29ce9016166713cbbae1e073
omadushika <madushikaoshan2018@gmail.com>
2022-10-15 12:19:32 +0530

    no message

---
+++ Task5/Task5.js
@@ -0,0 +1,25 @@
+var readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+
+readline.question('Please enter a number: ' , function (num){
+
+    if (num>=0) {
+        if(num % 2 === 0) {
+            console.log("Even number");
+        }
+        
+        else {
+            console.log("Odd number");
+        }
+        
+        
+    } else {
+        console.log("invalid number")
+    }
+    
+    readline.close();
+
+})
